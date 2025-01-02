e868ea2223cf388ad08e0f2d8443bb3a2f67eb72
marcozsh <marc.penar@outlook.cl>
2025-01-02 03:44:05 -0300

    fix text color

--- src/components/experience/timeline.tsx
+++ src/components/experience/timeline.tsx
@@ -4,14 +4,14 @@ type TimeLineProps = {
 
 export default function TimeLineComponent({ details }: TimeLineProps) {
   return (
-    <ol className="relative border-s border-gray-200 dark:border-gray-700">
+    <ol className="relative border-s border-gray-200">
       {details.map((detail, index) => (
         <li key={index} className="mb-10 ms-4">
           <div className="absolute w-3 h-3 rounded-full mt-[6px] -start-1.5 bg-custom_purple"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          <time className="mb-1 text-sm font-normal leading-none text-custom_gray opacity-[0.6]">
             {detail.date}
           </time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+          <h3 className="text-lg font-semibold">
             {detail.position}
           </h3>
           <p className="mb-4 text-base font-normal text-custom_gray opacity-[0.6]">
