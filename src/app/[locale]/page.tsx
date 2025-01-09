658c205b0d52e70cd1230e9bee0be4db5234d27c
marcozsh <marc.penar@outlook.cl>
2025-01-09 02:17:29 -0300

    fix not found bg, fix cv link, update opengraph img

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -19,17 +19,6 @@ export default function Home() {
   //unstable_setRequestLocale(locale);
   return (
     <main>
-      {/*Background credits to https://bg.ibelick.com/*/}
-      <div className="fixed left-0 top-0 -z-10 h-full w-full">
-        <div className="relative h-full w-full bg-slate-950">
-          <div
-            className={`absolute bottom-0 left-0 right-0 top-0 
-		bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
-		bg-[size:14px_24px]`}
-          ></div>
-        </div>
-      </div>
-      {/*Background*/}
       <Landing />
       <Projects />
       <Experience />
